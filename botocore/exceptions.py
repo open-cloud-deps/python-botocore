dea6f47712096194b7f3d07a81a92164ecd6ac5e
TANIGUCHI Takaki <takaki@asis.media-as.org>
2015-11-24 20:40:16 +0900

    Don't use duplicated modules
    fix path

--- botocore/exceptions.py
+++ botocore/exceptions.py
@@ -11,7 +11,7 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-from botocore.vendored.requests.exceptions import ConnectionError
+from requests.exceptions import ConnectionError
 
 
 class BotoCoreError(Exception):
